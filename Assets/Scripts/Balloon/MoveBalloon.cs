f653ba83f3a4f66f7a40a3aa5bc2915eab4211e5
Thyne <Thyneschool@gmail.com>
2026-02-13 20:08:40 +0100

    Gave the MainScene a better sound effect (selfRecorded)
    OMG IK HEB MIJN EERSTE ZELF GEMAAKTE SOUND EFFECT IN EEN GAME GESTOPT DIT IS ZO TUFF EN HYPER MODE!

--- Assets/Scripts/Balloon/MoveBalloon.cs
+++ Assets/Scripts/Balloon/MoveBalloon.cs
@@ -4,10 +4,16 @@ public class MoveBalloon : MonoBehaviour
 {
     public int health = 5;
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float maxHeight = 10f;
     void Update()
     {
         gameObject.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         CheckDeath();
+        if (transform.position.y >= maxHeight)
+        {
+            Destroy(gameObject);
+            //dmg doen aan de speler hier.
+        }
     }
 
     private void CheckDeath()
